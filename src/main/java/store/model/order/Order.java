e10e0a2c50b431bd150f926aa9441127766ee8e3
gitubanana <ptyolo666@gmail.com>
2024-11-09 21:39:22 +0900

    feat(Orders): 주문 유효성 검사 기능 추가
    Orders
    - 주문하는 상품명이 중복되면 안 된다.
    Order
    - 주문하는 상품의 수량은 음수가 될 수 없다.

--- src/main/java/store/model/order/Order.java
+++ src/main/java/store/model/order/Order.java
@@ -1,11 +1,40 @@
 package store.model.order;
 
+import static store.constant.ErrorMessage.WRONG_INPUT;
+
 public class Order {
     private final String productName;
-    private final int buyCount;
+    private int buyCount;
 
     public Order(String productName, final int buyCount) {
+        validate(buyCount);
         this.productName = productName;
         this.buyCount = buyCount;
+    }
+
+    private void validate(final int buyCount) {
+        validateNotNegative(buyCount);
+    }
+
+    private void validateNotNegative(final int buyCount) {
+        if (buyCount < 0) {
+            throw new IllegalArgumentException(WRONG_INPUT.getMessage());
+        }
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getBuyCount() {
+        return buyCount;
+    }
+
+    public void addBuyCount(final int toAdd) {
+        buyCount += toAdd;
+    }
+
+    public void subBuyCount(final int toSub) {
+        buyCount -= toSub;
     }
 }
--- src/main/java/store/model/order/Orders.java
+++ src/main/java/store/model/order/Orders.java
@@ -1,11 +1,29 @@
 package store.model.order;
 
+import static store.constant.ErrorMessage.DUPLICATED_ORDER;
+
 import java.util.List;
 
 public class Orders {
     private final List<Order> orders;
 
     public Orders(List<Order> orders) {
+        validate(orders);
         this.orders = orders;
+    }
+
+    private void validate(List<Order> orders) {
+        validateOrderNotDuplicated(orders);
+    }
+
+    private void validateOrderNotDuplicated(List<Order> orders) {
+        int distinctCount = (int) orders.stream().map(Order::getProductName).distinct().count();
+        if (distinctCount != orders.size()) {
+            throw new IllegalArgumentException(DUPLICATED_ORDER.getMessage());
+        }
+    }
+
+    public List<Order> toList() {
+        return List.copyOf(orders);
     }
 }
