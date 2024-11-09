713cb713181501ba32851583c85aa53201904045
gitubanana <ptyolo666@gmail.com>
2024-11-09 20:25:51 +0900

    feat(ErrorMessage): 에러 메시지 추가
    DUPLICATED_ORDER, WRONG_INPUT 에러 메시지를 추가한다.

--- src/main/java/store/constant/ErrorMessage.java
+++ src/main/java/store/constant/ErrorMessage.java
@@ -9,6 +9,8 @@ public enum ErrorMessage {
     EXCEEDED_QUANTITY("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
     BUY_COUNT_NOT_POSITIVE("증정 조건이 올바르지 않습니다."),
     GET_COUNT_NOT_POSITIVE("증정 개수가 올바르지 않습니다."),
+    DUPLICATED_ORDER("한 물건에 대한 주문이 중복됩니다. 한 곳에 모아서 써주세요."),
+    WRONG_INPUT("잘못된 입력입니다. 다시 입력해 주세요."),
     ;
 
     private static final String ERROR_PREFIX = "[ERROR] ";
