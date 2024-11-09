14af6b55441f1c986cb88366a27066f03f3e2da5
gitubanana <ptyolo666@gmail.com>
2024-11-09 22:01:02 +0900

    feat: 올바른 주문을 받는 기능
    Store를 사용해 들어온 주문에 대해서 존재하는 상품인지 재고가 있는지에 대해 판별한다.
    올바르지 않은 주문이 들어온다면, 다시 입력받는다.

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,17 +1,29 @@
 package store.controller;
 
-import static store.constant.FileInfo.PRODUCTS_FILE;
-import static store.constant.FileInfo.PROMOTIONS_FILE;
-import static store.constant.OutputMessage.WELCOME_MESSAGE;
-import static store.constant.PromptMessage.MAY_I_TAKE_YOUR_ORDER;
+import static store.constant.Answer.NO;
+import static store.constant.Answer.YES;
+import static store.constant.OutputInfo.MAY_I_TAKE_YOUR_ORDER;
+import static store.constant.OutputInfo.MEMBERSHIP_DISCOUNT;
+import static store.constant.OutputInfo.PRODUCT_START_MESSAGE;
+import static store.constant.OutputInfo.WANNA_BUY_MORE;
+import static store.constant.OutputInfo.WELCOME_MESSAGE;
+import static store.constant.StoreInfo.PRODUCTS_FILE;
+import static store.constant.StoreInfo.PROMOTIONS_FILE;
 
+import java.util.ArrayList;
+import java.util.List;
+import store.constant.Answer;
 import store.model.Store;
+import store.model.order.Order;
 import store.model.order.Orders;
 import store.model.product.Products;
 import store.model.product.ProductsLoader;
 import store.model.product.PromotionProducts;
 import store.model.promotion.Promotions;
 import store.model.promotion.PromotionsLoader;
+import store.model.receipt.PurchaseInfo;
+import store.model.receipt.PurchaseInfos;
+import store.model.receipt.Receipt;
 import store.util.Task;
 import store.view.InputView;
 import store.view.OutputView;
@@ -27,11 +39,11 @@ public class StoreController {
     }
 
     private Store loadFilesAndMakeStore() {
-        ProductsLoader productsLoader = new ProductsLoader(PRODUCTS_FILE);
         PromotionsLoader promotionsLoader = new PromotionsLoader(PROMOTIONS_FILE);
+        ProductsLoader productsLoader = new ProductsLoader(PRODUCTS_FILE,
+                new Promotions(promotionsLoader.getPromotions()));
 
         return new Store(
-                new Promotions(promotionsLoader.getPromotions()),
                 new Products(productsLoader.getProducts()),
                 new PromotionProducts(productsLoader.getPromotionProducts())
         );
@@ -39,14 +51,25 @@ public class StoreController {
 
     private void printWelcomeAndAllProducts(Store store) {
         OutputView.println(WELCOME_MESSAGE);
+        OutputView.println(PRODUCT_START_MESSAGE);
         OutputView.printProducts(store.getProducts());
         OutputView.printPromotionProducts(store.getPromotionProducts());
     }
 
     private Orders getOrders() {
+    private Orders readAvailableOrders(Store store) {
         return Task.retryTillNoException(() -> {
             OutputView.println(MAY_I_TAKE_YOUR_ORDER.getMessage());
             return new Orders(InputView.readOrders());
+            OutputView.println(MAY_I_TAKE_YOUR_ORDER);
+            Orders orders = new Orders(InputView.readOrders());
+
+            for (Order order : orders.toList()) {
+                store.checkAvailability(order);
+            }
+            return orders;
+        });
+    }
         });
     }
 }
--- src/main/java/store/model/Store.java
+++ src/main/java/store/model/Store.java
@@ -1,16 +1,20 @@
 package store.model;
 
+import static store.constant.ErrorMessage.CANNOT_FIND_PRODUCT;
+import static store.constant.ErrorMessage.EXCEEDED_QUANTITY;
+
+import store.model.order.Order;
+import store.model.product.Product;
 import store.model.product.Products;
+import store.model.product.PromotionProduct;
 import store.model.product.PromotionProducts;
-import store.model.promotion.Promotions;
+import store.model.receipt.PurchaseInfo;
 
 public class Store {
-    private final Promotions promotions;
     private final Products products;
     private final PromotionProducts promotionProducts;
 
-    public Store(Promotions promotions, Products products, PromotionProducts promotionProducts) {
-        this.promotions = promotions;
+    public Store(Products products, PromotionProducts promotionProducts) {
         this.products = products;
         this.promotionProducts = promotionProducts;
     }
@@ -22,4 +26,31 @@ public class Store {
     public PromotionProducts getPromotionProducts() {
         return promotionProducts;
     }
+
+    public void checkAvailability(Order order) {
+        checkProductName(order);
+        checkStock(order);
+    }
+
+    private void checkProductName(Order order) {
+        Product product = products.getCorrespondingTo(order);
+        if (product == null) {
+            throw new IllegalArgumentException(CANNOT_FIND_PRODUCT.getMessage());
+        }
+    }
+
+    private void checkStock(Order order) {
+        Product product = products.getCorrespondingTo(order);
+        PromotionProduct promotionProduct = promotionProducts.getCorrespondingTo(order);
+        int totalQuantity = product.getQuantity();
+
+        if (isAvailable(promotionProduct)) {
+            totalQuantity += promotionProduct.getQuantity();
+        }
+
+        if (totalQuantity < order.getBuyCount()) {
+            throw new IllegalArgumentException(EXCEEDED_QUANTITY.getMessage());
+        }
+    }
+
 }
