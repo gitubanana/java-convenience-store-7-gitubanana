38930047412d526591a494eec1fa1f78f8f0b409
gitubanana <ptyolo666@gmail.com>
2024-11-11 18:14:31 +0900

    refactor(Converter): InputView에 있는 메서드 Converter로 위치 변경

--- src/main/java/store/util/Converter.java
+++ src/main/java/store/util/Converter.java
@@ -1,10 +1,16 @@
 package store.util;
 
+import static store.constant.ErrorMessage.INVALID_ORDER_FORMAT;
 import static store.constant.ErrorMessage.WRONG_INPUT;
+import static store.constant.OrderInfo.ORDER_PATTERN;
+import static store.constant.OrderInfo.PRODUCT_COUNT_GROUP;
+import static store.constant.OrderInfo.PRODUCT_NAME_GROUP;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import store.model.order.Order;
 
 public class Converter {
     public static int toInteger(String number) {
@@ -21,5 +27,18 @@ public class Converter {
                         DateTimeFormatter.ofPattern(dateFormat)
                 )
                 .atStartOfDay();
+    }
+
+    public static Order toOrder(String order) {
+        Matcher matcher = ORDER_PATTERN.matcher(order);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(INVALID_ORDER_FORMAT.getMessage());
+        }
+
+        return new Order(
+                matcher.group(PRODUCT_NAME_GROUP),
+                Converter.toInteger(matcher.group(PRODUCT_COUNT_GROUP))
+        );
     }
 }
--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,16 +1,11 @@
 package store.view;
 
-import static store.constant.ErrorMessage.INVALID_ORDER_FORMAT;
 import static store.constant.ErrorMessage.WRONG_INPUT;
 import static store.constant.OrderInfo.ORDER_DELIMITER;
-import static store.constant.OrderInfo.ORDER_PATTERN;
-import static store.constant.OrderInfo.PRODUCT_COUNT_GROUP;
-import static store.constant.OrderInfo.PRODUCT_NAME_GROUP;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import store.constant.Answer;
 import store.model.order.Order;
 import store.util.Converter;
@@ -24,22 +19,9 @@ public class InputView {
         while (spliter.hasMoreTokens()) {
             String nextToken = spliter.nextToken();
 
-            orders.add(makeOrder(nextToken));
+            orders.add(Converter.toOrder(nextToken));
         }
         return orders;
-    }
-
-    private static Order makeOrder(String order) {
-        Matcher matcher = ORDER_PATTERN.matcher(order);
-
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException(INVALID_ORDER_FORMAT.getMessage());
-        }
-
-        return new Order(
-                matcher.group(PRODUCT_NAME_GROUP),
-                Converter.toInteger(matcher.group(PRODUCT_COUNT_GROUP))
-        );
     }
 
     public static Answer readAnswer() {
