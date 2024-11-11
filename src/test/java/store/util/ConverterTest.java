b9b19f7edd1f29de0650b494ab9b5436f9abb4c2
gitubanana <ptyolo666@gmail.com>
2024-11-11 21:55:49 +0900

    test(Converter): toOrder 단위 테스트 추가

--- src/test/java/store/util/ConverterTest.java
+++ src/test/java/store/util/ConverterTest.java
@@ -3,6 +3,7 @@ package store.util;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static store.constant.ErrorMessage.INVALID_ORDER_FORMAT;
 import static store.constant.ErrorMessage.WRONG_INPUT;
 
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import store.model.order.Order;
 
 @DisplayName("== Converter 테스트 ==")
 public class ConverterTest {
@@ -63,6 +65,41 @@ public class ConverterTest {
                 Arguments.of("1945/08/15", 1945, 8, 15),
                 Arguments.of("1953/07/27", 1953, 7, 27),
                 Arguments.of("2024/11/11", 2024, 11, 11)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("주문 형식에 맞지 않을 경우 예외를 발생시킬 수 있다.")
+    @ValueSource(strings = {"[]", "[hi-hi]", ""})
+    void 주문_형식_예외(String order) {
+        assertThatThrownBy(() -> Converter.toOrder(order))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_ORDER_FORMAT.getMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("주문 제품과 수량을 파싱할 수 있다.")
+    @MethodSource("getToOrderArguments")
+    void 제품_수량_파싱(String orderString, String productName, final int buyCount) {
+        Order order = Converter.toOrder(orderString);
+
+        assertAll(
+                () -> {
+                    assertThat(order.getProductName())
+                            .isEqualTo(productName);
+                },
+                () -> {
+                    assertThat(order.getBuyCount())
+                            .isEqualTo(buyCount);
+                }
+        );
+    }
+
+    static Stream<Arguments> getToOrderArguments() {
+        return Stream.of(
+                Arguments.of("[껌-5]", "껌", 5),
+                Arguments.of("[김밥-7]", "김밥", 7),
+                Arguments.of("[콜라-10]", "콜라", 10)
         );
     }
 }
